c5797ec98fb1c07003932144649791132d5c4444
Anton Tishin <atishin93@gmail.com>
2017-03-07 12:08:06 +0600

    last changes

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleApplication1
         static Printer printer = new Printer();
         static void Main(string[] args)
         {
-            var signalrHost = "http://localhost:59874/";
+            var signalrHost = "http://212.19.138.131:7808/";
             var operator_name = "Поликлиника №30";
             if (args.Length != 0)
             {
@@ -28,68 +28,19 @@ namespace ConsoleApplication1
                 operator_name = args[1];
             }
 
-            var printer = new Printer()
-            {
-                mo_name = "Поликлиника №30",
+            var printer = new Printer();
+            printer.mo_name = "Поликлиника №30";
+            SignalRProvider sProvider = new SignalRProvider() {
+                host = signalrHost,
+                printer = printer,
                 operator_name = operator_name
             };
-
-            ConnectSignalR(signalrHost, operator_name);
+            sProvider.Connect();
+            while (true) {
+                var a = Console.ReadKey();
+                if (a.Key == ConsoleKey.Q) return;
+            }
         }
-
-        static void ConnectSignalR(string host, string operator_name)
-        {
-            var connection = new HubConnection(host);
-            //Make proxy to hub based on hub name on server
-            var myHub = connection.CreateHubProxy("VisitsHub");
-            //Start connection
-
-            connection.Start().ContinueWith(task => {
-                if (task.IsFaulted)
-                {
-                    Console.WriteLine("There was an error opening the connection:{0}",
-                                      task.Exception.GetBaseException());
-                }
-                else
-                {
-                    Console.WriteLine("Connected");
-                }
-
-            }).Wait();
-
-            myHub.On<dynamic, int>("ticketNotify", (param, act) => {
-                if (act == 0)
-                {
-                    DateTime date = param.RegisteredOn;
-                    printer.date = date.ToString("dd.MM.yyyy");
-                    printer.time = date.ToString("hh:mm");
-
-                    printer.ticket = param.TicketNumber;
-
-                    printer.patient_line_1 = param.Patient.Person.LastName;
-                    printer.patient_line_2 = param.Patient.Person.FirstName;
-                    printer.patient_line_3 = param.Patient.Person.MidleName;
-
-                    printer.doctor_line_1 = param.Employee.Person.LastName;
-                    printer.doctor_line_2 = param.Employee.Person.FirstName;
-                    printer.doctor_line_3 = param.Employee.Person.MidleName;
-                    printer.mo_name = "Поликлиника №30";
-                    printer.operator_name = operator_name;
-                    if (param.Employee.Cabinet != null)
-                    {
-                        printer.cabinet = param.Employee.Cabinet.Title;
-                    }
-                    printer.Print();
-                    Console.WriteLine(param);
-                }
-                
-            });
-
-            Console.Read();
-            connection.Stop();
-        }
-
-        
     }
 
 }
---
+++ ConsoleApplication1/SignalRProvider.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class SignalRProvider
+    {
+
+        public string host { get; set; }
+        public string operator_name { get; set; }
+
+        public Printer printer { get; set; }
+
+        private HubConnection connection { get; set; }
+
+        ~SignalRProvider()
+        {
+            connection.Stop();
+        }
+
+        public void Connect()
+        {
+            connection = new HubConnection(host);
+            var myHub = connection.CreateHubProxy("VisitsHub");
+
+            connection.Start().ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+            myHub.On<dynamic, int>("ticketNotify", (param, act) => {
+                if (act == 0)
+                {
+                    DateTime date = param.RegisteredOn;
+                    printer.date = date.ToString("dd.MM.yyyy");
+                    printer.time = date.ToString("hh:mm");
+
+                    printer.ticket = param.TicketNumber;
+
+                    printer.patient_line_1 = param.Patient.Person.LastName;
+                    printer.patient_line_2 = param.Patient.Person.FirstName;
+                    printer.patient_line_3 = param.Patient.Person.MidleName;
+
+                    printer.doctor_line_1 = param.Employee.Person.LastName;
+                    printer.doctor_line_2 = param.Employee.Person.FirstName;
+                    printer.doctor_line_3 = param.Employee.Person.MidleName;
+                    printer.mo_name = "Поликлиника №30";
+                    printer.operator_name = operator_name;
+                    if (param.Employee.Cabinet != null)
+                    {
+                        printer.cabinet = param.Employee.Cabinet.Title;
+                    }
+                    printer.Print();
+                    //Console.WriteLine(param);
+                }
+
+            });
+        }
+    }
+}
