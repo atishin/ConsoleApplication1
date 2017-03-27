288c64b0918236ae3c080c1c272be3793ec9f715
Chingiz <olzhabaev.sh@outlook.com>
2017-03-27 20:03:02 +0600

    update

--- KaizenTalonClient/Program.cs
+++ KaizenTalonClient/Program.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TalonService;
 
 namespace KaizenTalonClient
 {
     static class Program
     {
+        private static Printer _printer;
+        public static Printer Printer
+        {
+            get
+            {
+                if (_printer == null)
+                {
+                    _printer = new Printer();
+                }
+
+                return _printer;
+            }
+        }
+
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
